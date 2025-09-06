1c07911e51e55c22c8f7f792b484c8177456eb2f
Luka Mshvildadze <160271572+MshvildadzeLuka@users.noreply.github.com>
2025-09-06 21:13:30 +0400

    Update auth.js

--- client/assets/js/auth.js
+++ client/assets/js/auth.js
@@ -1,16 +1,29 @@
-// client/assets/js/auth.js
+/**
+ * GLOBAL AUTHENTICATION SCRIPT
+ * ----------------------------
+ * This script solves the "No Global Authentication Enforcement" problem.
+ * It runs on every page (except login) to ensure that only authenticated
+ * users can view the content.
+ *
+ * It should be included in the <head> of each HTML file to execute
+ * before the page content is rendered.
+ */
 
 (() => {
+  // Retrieve the authentication token from localStorage.
+  // The login page will set this token upon successful login.
   const token = localStorage.getItem('piRateToken');
-  const currentPage = window.location.pathname;
-  
-  // Checks if the current page is the login page.
-  const isLoginPage = currentPage.endsWith('/login/html');
 
-  // If there is no token AND the user is not currently on the login page,
-  // they are not authorized and will be redirected.
+  // Get the current page's path.
+  const currentPage = window.location.pathname;
+
+  // Check if the user is on the login page.
+  const isLoginPage = currentPage.endsWith('login.html');
+
+  // If there is no token AND the user is not on the login page,
+  // they are not authorized. Redirect them to the login page.
   if (!token && !isLoginPage) {
-    // This absolute path works from anywhere in your site.
-    window.location.href = '/login/login.html';
+    // The path must be absolute from the root to work reliably from any page.
+    window.location.href = '/client/login/login.html';
   }
 })();
